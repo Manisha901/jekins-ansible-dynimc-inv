e1e74fa71af22bbf42fd0a936f3c7f4fc6cdc449
Manisha901 <manishaputta901@gmail.com>
2025-04-24 22:18:29 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -5,7 +5,7 @@ region = "ap-south-1"
 resource "aws_instance" "AWSEC2Instance"{
  ami = "ami-0f1dcc636b69a6438"
  instance_type = "t2.micro"
- key_name = "devopsjan2025"
+ key_name = "devops"
  security_groups = ["launch-wizard-2"]
  tags={
    Name = "TerraformServer"
